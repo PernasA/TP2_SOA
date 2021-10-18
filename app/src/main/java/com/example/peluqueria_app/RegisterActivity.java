ba2c97666033b73f5f2b474abd48f6fb984e088f
PernasA <agustinbaltazarpernas@gmail.com>
2021-10-18 19:22:18 -0300

    intent a register working

---
+++ app/src/main/java/com/example/peluqueria_app/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.example.peluqueria_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+    }
+
+}
